b48aca21b4b919b4dbd5c71ff9e0eac1c180b2c7
Aravind Guntha <aravind.guntha.4107@gmail.com>
2024-07-28 23:50:43 +0530

    Bug Fixed CalledContract.sol

--- CalledContract.sol
+++ CalledContract.sol
@@ -8,11 +8,12 @@ contract CalledContract{
 
   uint256 NextyearAge;
 
-  function IncrementAge(uint _age) public retruns(uint256 _AgeOut){
+  function IncrementAge(uint _age) public returns(uint256 _AgeOut)
+  {
 
-    Age = _age;
+    _AgeOut = _age;
     NextyearAge = _age + 1;
+    return _AgeOut;
   
   }
-
 }
